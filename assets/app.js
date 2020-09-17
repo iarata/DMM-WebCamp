e066f3fdfaa38821a62512a9edd0b0c7bec53f81
Alireza Hajebrahimi <mrex778@yahoo.ca>
2020-09-17 19:39:41 +0900

    improved app.js

--- assets/app.js
+++ assets/app.js
@@ -22,32 +22,56 @@
 // }
 // document.getElementById("result").innerHTML = userresult;
 
-(function() {
+(function () {
     // Selecting the input element
     const form = document.querySelector('.nameForm')
     //Set up Submit Button
-    form.addEventListener('submit', function(e){
+    form.addEventListener('submit', function (e) {
+        
         // prevent the form's default submission action
         e.preventDefault()
+
         //Get user's input from from
-        const userName = document.querySelector('.nameField')
-        const feedback = document.querySelector('.feedback')
-        const welcomeName = document.querySelector('.nameContent')
-        const displayName = document.querySelector('#name')
-    
-        if (userName.value === ''){
+        const userName      = document.querySelector('.nameField')
+        const displayName   = document.querySelector('#name')
+
+        const feedback      = document.querySelector('.feedback')
+
+        const omikujiResult = document.querySelector('.result')
+
+        if (userName.value === '') {
             feedback.classList.add('show')
-            setTimeout(function(){
-            feedback.classList.remove('show')
+            setTimeout(function () {
+                feedback.classList.remove('show')
             }, 2000)
             if (displayName.value != '') {
-                welcomeName.classList.remove('show')
+                omikujiResult.classList.remove('show')
             }
         } else {
             //Change message content and clear the message input
-            welcomeName.classList.add('show')
+            omikujiResult.classList.add('show')
             displayName.textContent = userName.value
             userName.value = ''
+
+
+            var results = ''
+            var rand = Math.floor(Math.random() * 5);
+            if (rand == 0) {
+                results = "大吉";
+            }
+            if (rand == 1) {
+                results = "中吉";
+            }
+            if (rand == 2) {
+                results = "小吉";
+            }
+            if (rand == 3) {
+                results = "吉";
+            }
+            if (rand == 4) {
+                results = "凶";
+            }
+            document.getElementById("omikujiText").innerHTML = results
         }
     })
-    })()
+})()
